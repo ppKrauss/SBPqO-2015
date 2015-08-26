044f6cd102a3c44fe20197708486377ef28ba8ae
ppkrauss <ppkrauss@gmail.com>
2015-08-26 10:06:38 -0300

    ops

--- src/php/main.php
+++ src/php/main.php
@@ -4,7 +4,7 @@
  * USO:
  *  $ cd projeto
  *  $ php src/php/main.php  --raw --in=entregas/HTM1 > entregas/HTM2/resumosOriginais.html
- *  $ php src/php/main.php  --finalXml --in=entregas/HTM1 > all1.xml
+ *  $ php src/php/main.php  --finalXml --multisec < entregas/HTM2/resumosOriginais.html > all1.xml
  *  $ php src/php/main.php  --finalHtml < all1.xml > all1.htm
  *  $ php tools/main.php --finalXml --day=2014-09-03  --in=material/originais-UTF8/ > SBPqO_dia09-03.xml
  *  $ php tools/main.php -h
